d5b5ce1a9e00f02a03d98182064e4917ad85bdb9
marcelpierres <marcelpierre.samuels@ryerson.ca>
2018-08-07 02:05:17 -0400

    Base v2

--- repertoire/src/components/me.jsx
+++ repertoire/src/components/me.jsx
@@ -1,28 +1,73 @@
 import React, { Component } from "react";
+import profpic from "./images/profpic1.png";
+import email from "./images/email.png";
+import github from "./images/github.png";
+import linkedin from "./images/linked1.png";
+import resume from "./images/resume.png";
+import resumepdf from "./images/marcelpierreresume.pdf";
 import "./styling/me.css";
 
 class Me extends Component {
   state = {};
 
-  centercontainer = {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center"
-  };
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-centered">
             <div className="card bg-light mb-3">
-              <div className="card-header">Header</div>
+              <div className="card-header">
+                <img className="profpic" src={profpic} />
+              </div>
               <div className="card-body">
-                <h5 className="card-title">Light card title</h5>
+                <h2 className="name">Marcel-Pierre Samuels</h2>
+                <h4 className="status">Computer Science Graduate</h4>
                 <p className="card-text">
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
+                <a className="images" href="mailto:marcelpierre.s@gmail.com">
+                  <img
+                    id="mail"
+                    src={email}
+                    alt="email"
+                    height="50"
+                    width="50"
+                  />
+                </a>
+
+                <a className="images" href="https://github.com/marcelpierres">
+                  <img
+                    id="github"
+                    src={github}
+                    alt="github"
+                    height="50"
+                    width="50"
+                  />
+                </a>
+
+                <a
+                  className="images"
+                  href="https://linkedin.com/in/marcel-pierre-samuels-332525163"
+                >
+                  <img
+                    id="linkedin"
+                    src={linkedin}
+                    alt="linkedin"
+                    height="50"
+                    width="50"
+                  />
+                </a>
+
+                <a className="images" href={resumepdf}>
+                  <img
+                    id="resume"
+                    src={resume}
+                    alt="resume"
+                    height="50"
+                    width="50"
+                  />
+                </a>
               </div>
             </div>
           </div>
